0548fe4c46ff5e1aadb5500240ab7d1e57930d56
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-11-17 23:15:20 +0900

    [Remove] ロゴやCSSファイルを削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
-import { useState } from 'react';
-import './App.css';
-
 export const App = () => {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <h1>Hello.</h1>
-      <button onClick={() => setCount(count + 1)}>count: {count}</button>
+      <h1>Hello World</h1>
     </div>
   );
 };
